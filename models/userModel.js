8ae35284e11beae82d90ede46299e3c7a6892752
Yarizk <yazidrizkik@gmail.com>
2022-11-06 20:38:07 +0700

    match email syntax

--- models/userModel.js
+++ models/userModel.js
@@ -2,12 +2,19 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
 var userSchema = new Schema({
     // email username password required
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
     },
     username: {
         type: String,
--- routes/registerRoute.js
+++ routes/registerRoute.js
@@ -1,41 +1,51 @@
 //
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/userModel');
-const bcrypt = require('bcrypt');
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
-
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
 //register
 router.route("/").post((req, res) => {
-    const username = req.body.username;
-    const email = req.body.email;
-    const password = req.body.password;
-
-    bcrypt.hash(password, saltRounds, (err, hash) => {
-        if (err) {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+    
+  // check if email exist
+  User.findOne({ email: email }, (err, foundUser) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        res.send("Email already exist");
+      } else {
+        bcrypt.hash(password, saltRounds, (err, hash) => {
+          if (err) {
             console.log(err);
-        }
+          }
 
-        const newUser = new User({
+          const newUser = new User({
             username: username,
             email: email,
-            password: hash
-        });
+            password: hash,
+          });
 
-        newUser
+          newUser
             .save()
             .then((data) => {
-                res.json(data);
+              res.json(data);
             })
             .catch((error) => {
-                res.json(error);
+              res.json(error);
             });
-    });
+        });
+      }
+    }
+  });
 });
 
 module.exports = router;
