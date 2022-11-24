f76daa0a94f3416be065c97fb577e5d2d9c761ff
Yarizk <yazidrizkik@gmail.com>
2022-11-24 17:29:37 +0700

    cors

--- server.js
+++ server.js
@@ -26,8 +26,14 @@ mongoose.connection.on('connected', () => {
 });
 
 //cors
-const cors = require('cors');
-app.use(cors());
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) 
 
 //routes
 const routes = require('./routes/router');
